743b3f06b0155aeb8b19bf909fcc8ce4e455cf3e
Samia <89767108+Samia-ezzat@users.noreply.github.com>
2024-08-30 22:11:14 +0300

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,13 @@
+//swiper
+
+var swiper = new Swiper (".home", {
+  spaceBetween:30,
+  centerdSlides: true,
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
